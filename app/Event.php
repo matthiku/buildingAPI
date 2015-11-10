80c5ea83ecda330ef5cda6e5acce1d7a39609062
Matthias Kuhs <matthiku@gmail.com>
2015-11-10 10:29:33 +0000

    adding further methods and cleanup

--- app/Event.php
+++ app/Event.php
@@ -11,11 +11,11 @@ class Event extends Model
 
 
     // fields that cannot be changed via the API
-    protected $hidden = [ 'id', 'seed', 'ipaddr', 'created_at', 'updated_at' ];
+    protected $hidden = [ 'seed', 'ipaddr', 'created_at', 'updated_at' ];
 
     // fields that can be changed via the API
     protected $fillable = [
-        'status', 'weekday', 'start', 'end', 
+        'id', 'status', 'weekday', 'start', 'end', 
         'title', 'repeats', 'nextdate', 'rooms', 'targetTemp' 
     ];
 
